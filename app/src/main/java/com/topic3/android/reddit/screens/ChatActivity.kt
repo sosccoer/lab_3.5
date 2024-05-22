f63861c4cc7a061129ff9f07d6d5980007693ef3
VrodeYuliya <romanova.yulia5444@gmail.com>
2024-05-22 22:09:40 +0300

    update chatActivity.kt

--- app/src/main/java/com/topic3/android/reddit/screens/ChatActivity.kt
+++ app/src/main/java/com/topic3/android/reddit/screens/ChatActivity.kt
@@ -5,6 +5,21 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Button
 import com.topic3.android.reddit.databinding.ActivityChatBinding
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ButtonDefaults.buttonColors
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.util.Locale
 
 class ChatActivity : AppCompatActivity() {
 
@@ -28,4 +43,33 @@ class ChatActivity : AppCompatActivity() {
     private fun showToast() {
         Toast.makeText(this, "Imaginary chat started!", Toast.LENGTH_SHORT).show()
     }
+
+}
+@Composable
+private fun ComposeButton(onButtonClick: () -> Unit) {
+    val buttonColors = buttonColors(
+        backgroundColor = Color(0xFF006837),
+        contentColor = Color.White
+    )
+
+    Button(
+        onClick = onButtonClick, elevation = null,
+        shape = RoundedCornerShape(corner = CornerSize(24.dp)),
+        contentPadding = PaddingValues(
+            start = 32.dp, end = 32.dp
+        ),
+        colors = buttonColors,
+        modifier = Modifier.height(48.dp)
+    ) {
+        Text(
+            text = "Start chatting".toUpperCase(Locale.US),
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium
+        )
+    }
+}
+@Preview(showBackground = true)
+@Composable
+private fun ComposeButtonPreview() {
+    ComposeButton {}
 }
